1296ed7b3e57420cea6e74817902691e439e3eaa
Mayank Pal <mayankpal9654@gmail.com>
2025-02-22 15:59:38 +0530

    adding searching and  complexities

---
+++ search/linearsearch/SearchWord.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.dsa.search.linearsearch;
+
+public class SearchWord {
+
+    static void searchWord(String[] array, String target){
+
+        for(String line : array){
+            if(line.contains(target)){
+                System.out.println(line);
+                return;
+            }
+        }
+
+        System.out.println("Not Found");
+
+    }
+
+    public static void main(String[] args) {
+        String[] sentences = {
+                "Mayank Pal , Arnav, Vibhor, Kanika",
+                "Java is a widely used programming language.",
+                "The quick brown fox jumps over the lazy dog.",
+                "Data structures and algorithms are important."
+        };
+
+        String target = "quick";
+
+        searchWord(sentences, target);
+
+
+    }
+}
